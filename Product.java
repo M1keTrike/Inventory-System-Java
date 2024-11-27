4f6671f29eff9e07a9e62328631a0d62a887f8d9
M1keTrike <miguemolina4570@gmail.com>
2024-11-27 13:59:21 -0600

    [ADDED] product class

---
+++ Product.java
@@ -0,0 +1,30 @@
+public class Product {
+    private String name;
+    private int stock;
+    private float price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+}
